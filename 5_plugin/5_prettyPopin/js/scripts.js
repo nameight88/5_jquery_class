f235d1844b1e85752490926643bf96bd48958c1e
nameight88 <david0108@naver.com>
2024-04-17 17:44:47 +0900

    4/17 PM

--- 5_plugin/5_prettyPopin/js/scripts.js
+++ 5_plugin/5_prettyPopin/js/scripts.js
@@ -4,11 +4,11 @@
 $(function(){
   
     // 첫번째 <a>에 prettyPopin
-    $('a[rel="prettyPopin"]:eq(0)').ptettyPopin({
+    $('a[rel="prettyPopin"]:eq(0)').prettyPopin({
         width: 500
     });
 
-    $('a[rel="prettyPopin"]:eq(1)').ptettyPopin({
+    $('a[rel="prettyPopin"]:eq(1)').prettyPopin({
         width: 500
     });
 })
