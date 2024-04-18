e0b921f45d11d8bfb01345c9c55c586c8ea83c87
nameight88 <david0108@naver.com>
2024-04-18 09:23:33 +0900

    4/17 PM

--- 5_plugin/9_namdawit/js/menu.js
+++ 5_plugin/9_namdawit/js/menu.js
@@ -1,15 +1,56 @@
 $(function(){
+    // 주문 내역에 메뉴를 추가하는 함수
+    function addToOrder(menuName, price) {
+        let name = $('<td>').text(menuName);
+        let priceCell = $('<td>').text(price);
+        let del = $('<td>').html('<button class="del">삭제</button>'); 
+        let newRow = $('<tr>').append(name, priceCell, del); 
+        $('#orderList').append(newRow);
+        updateTotalPrice(); // 총합 업데이트
+    }
+    $("#ad").bxSlider({
+        minSlides : 4
+        ,maxSlides : 4
+        ,slideMargin :10
+        ,slideWidth :90
+        ,moveSlides :1
+        ,auto : true
+      });
+
+    // 총합을 업데이트하는 함수
+    function updateTotalPrice() {
+        let totalPrice = 0;
+        $('#orderList tbody tr').each(function() {
+            let price = parseInt($(this).find('td:eq(1)').text());
+            totalPrice += price;
+            return totalPrice;
+        });
+        $('#result').text( totalPrice );
+    }
+
+    // 담기 버튼 클릭 시 주문 내역에 추가
     $('.menuadd').click(function(){
-    let name = $('<td>'+$('.label').val()+'</td>');
-    let price = $('<td>'+$('.price').val()+'</td>');
-    let del = $('<td><button class="del">삭제</button></td>'); 
-    let newRow = $('<tr>').append(name, price,del); 
-    $('#orderList').append(newRow);
-    $('.del').click(function() {
-        $(this).closest('tr').remove(); 
+        let menuName = $(this).siblings('.label').text();
+        let price = parseInt($(this).siblings('.price').data('price'));
+        addToOrder(menuName, price);
     });
-})
-   
+
+    // 삭제 버튼 클릭 시 해당 행 삭제
+    $(document).on('click', '.del', function() {
+        $(this).closest('tr').remove();
+        updateTotalPrice(); // 총합 업데이트
+    });
+
+    // 주문내역 보기 버튼 클릭 시 주문 내역 창 토글
+    $('#orderview').click(function() {
+        $('#rightTable').toggle();
+    });
+
+    $('#adview').click(function() {
+        $('.footerTable').toggle();
+    });
     
+
     
-})
+
+});
